ab79d78b520002aac75c3fee8df632e44d470f00
Luiz Gasparetto <luizegasp@gmail.com>
2022-10-05 23:15:58 -0300

    changed error name

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     return response.status(err.statusCode).json({ message: err.message });
   }
 
-  return response.status(500).json({ message: "Unknown server error", error: err.message });
+  return response.status(500).json({ message: "Internal Server Error", error: err.message });
 });
 
 app.listen(process.env.PORT, () => console.log("Server is running..."));
